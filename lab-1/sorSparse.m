bc1d2e125d9190d2febcc0b3471ec18a4ee7e9e0
Sliwson <mateusz_sliw@interia.pl>
2018-10-04 18:25:03 +0200

    Finish basic outline of method

--- lab-1/sorSparse.m
+++ lab-1/sorSparse.m
@@ -7,9 +7,9 @@ function x = sorSparse(A,b,w)
 %check diagonal elements
 diagonal = zeros(1,n);
 
-[~, i] = size(A);
+[~, k] = size(A);
 
-for a = 1:i
+for a = 1:k
     if A(1,a) == A(2,a)
         diagonal(A(1,a)) = A(3,a);
     end
@@ -17,14 +17,44 @@ end
 
 if(any(diagonal == 0))
     error("Diagonal element could not equal 0!");
+end
+
+%here we know that we have at least one nonzero element in each row    
+rowSplit = zeros(1,n);
+rowBeginnings = zeros(1,n);
+
+for a = 1:k
+    rowSplit(A(1,a)) = rowSplit(A(1,a)) + 1;
+end
+
+rowBeginnings(1) = 1;
+for a = 2:n
+    rowBeginnings(a) = rowBeginnings(a-1)+rowSplit(a-1);
+end
 
 x = ones(n,1);
 xnext = ones(n,1);
 
-for k = 1:10
-    for i = 1:10
-        %xnext(i) = (1-w)*x(i) + w/
+for k = 1:100
+    for i = 1:n
+        sum1=0;
+        sum2=0;
+        
+        for j = rowBeginnings(i):rowBeginnings(i)+rowSplit(i)
+            column = A(2,j);
+            value = A(3,j);
+            
+            if(column < i)
+                sum1 = sum1 + value*xnext(column);
+            elseif (column > i)
+                sum2 = sum2 + value*x(column);
+            end
+        end
+        
+        xnext(i) = (1-w)*x(i) + w/diagonal(i)*(b(i)- sum1 - sum2);
     end
+    
+    x = xnext;
 end
 
 end
