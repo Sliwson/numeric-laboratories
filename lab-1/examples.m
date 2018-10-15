1a246de8373367f55e9f7f65ff195e5e477013d0
Sliwson <mateusz_sliw@interia.pl>
2018-10-15 17:17:25 +0200

    Rewrite examples

--- lab-1/examples.m
+++ lab-1/examples.m
@@ -1,9 +1,22 @@
-A=[1,1,1,2,2,2,3,3,3;1,2,3,1,2,3,1,2,3;3,-1,1,-1,3,-1,1,-1,3];
-b = [-1;7;-7];
-w = 1.25;
-xFirst = [0;0;0];
-tolerance = 10e-6;
-maxIterations = 1e5;
-
-disp("answer:");
-disp(sorSparse(A,b,w,xFirst,tolerance,maxIterations));
+%mala macierz (5x5)
+A = [4,0,0,0,3;
+     3,5,0,0,2;
+     0,0,3,0,0;
+     2,0,0,6,0;
+     1,0,0,0,1];
+ 
+b = [1;2;3;4;5];
+ 
+displayComparison(@sorSparse, @sorNormal, A, b, 1.25);
+ 
+%sredniej wielkosci macierz (14x14)
+A = generateSquareMatrix(14, 0.95, 3, 10);
+b = 0.5 - rand(14,1);
+ 
+displayComparison(@sorSparse, @sorNormal, A, b, 1.25);
+ 
+%duza macierz (20000x20000)
+A = generateSquareMatrix(1000, 0.97, 1, 200);
+b = 0.5 - rand(1000,1);
+ 
+displayComparison(@sorSparse, @sorNormal, A, b, 1.25);
