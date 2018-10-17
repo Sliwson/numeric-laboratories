f688b037a2e83f2f2fcd6a4ce7105424d2a76c80
Sliwson <mateusz_sliw@interia.pl>
2018-10-17 12:36:43 +0200

    Rebuild comparison function not to count 2 times

--- lab-1/displayComparison.m
+++ lab-1/displayComparison.m
@@ -6,41 +6,48 @@ function displayComparison(handle1,handle2, A, b, w)
 [n,~] = size(A);
 ASparse = matrixToSparse(A);
 
-[x1,k1] = handle1(ASparse, b, w);
-
-handle = @() handle1(ASparse, b, w);
-t1 = timeit(handle);
-
-[x2,k2] = handle2(A, b, w);
-
-handle = @() handle2(A, b, w);
-t2 = timeit(handle);
-
-handle = @() A\b;
-x3 = handle();
-t3 = timeit(handle);
-
 disp('========================================================================================');
 D = ['Porownanie dla macierzy ', num2str(n), 'x', num2str(n)];
 disp(D);
+disp('Metoda SOR dla macierzy rozrzedzonej:');
+
+tic
+[x1,k1] = handle1(ASparse, b, w);
+t1 = toc;
+
+D = ['Liczba iteracji: ', num2str(k1)];
+disp(D);
+D = ['Czas: ', num2str(t1)];
+disp(D);
+
+disp('Zwykla metoda SOR:');
+
+tic
+[x2,k2] = handle2(A, b, w);
+t2 = toc;
+
+D = ['Liczba iteracji: ', num2str(k2)];
+disp(D);
+D = ['Czas: ', num2str(t2)];
+disp(D);
+
+disp('Wbudowana funkcja matlaba: ');
+
+tic
+x3 = A\b;
+t3 = toc;
+
+D = ['Czas: ', num2str(t3)];
+disp(D);
+
 D = ['Porownanie wynikow (norma z roznicy wynikow metod SOR): ', num2str(norm(x1-x2))];
 disp(D);
 D = ['Porownanie wynikow (norma z roznicy metody SOR i wbudowanej funkcji matlaba): ', num2str(norm(x1-x3))];
 disp(D);
-disp('Metoda SOR dla macierzy rozrzedzonej:');
-D = ['Czas: ', num2str(t1)];
+
+D = ['Iloraz czasow metody SOR oraz metody SOR dla macierzy rozrzedzonych: ', num2str(t2/t1)];
 disp(D);
-D = ['Liczba iteracji: ', num2str(k1)];
-disp(D);
-disp('Zwykla metoda SOR:');
-D = ['Czas: ', num2str(t2)];
-disp(D);
-D = ['Liczba iteracji: ', num2str(k2)];
-disp(D);
-disp('Wbudowana funkcja matlaba: ');
-D = ['Czas: ', num2str(t3)];
-disp(D);
-D = ['Iloraz czasow metod SOR: ', num2str(t2/t1)];
+D = ['Iloraz czasow wbudowanej funkcji matlaba oraz metody SOR dla macierzy rozrzedzonych: ', num2str(t3/t1)];
 disp(D);
 
 end
