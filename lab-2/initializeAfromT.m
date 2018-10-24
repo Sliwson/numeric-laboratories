7aa3bb4fb75dc96cbadc210291e0b3e7099f6b41
Sliwson <mateusz_sliw@interia.pl>
2018-10-24 13:51:15 +0200

    Complete first version of project

---
+++ lab-2/initializeAfromT.m
@@ -0,0 +1,12 @@
+function A = initializeAfromT(T,X,Y)
+%INITIALIZEAFROMT Summary of this function goes here
+%   Detailed explanation goes here
+x1 = X(T(1),T(2));
+x2 = X(T(3),T(4));
+x3 = X(T(5),T(6));
+y1 = Y(T(1),T(2));
+y2 = Y(T(3),T(4));
+y3 = Y(T(5),T(6));
+A = [x1, y1, x2, y2, x3, y3, (x1+x2)/2, (y1+y2)/2,(x2+x3)/2, (y2+y3)/2, (x1+x3)/2, (y1+y3)/2];
+end
+
--- lab-2/squareInterpolation.m
+++ lab-2/squareInterpolation.m
@@ -3,18 +3,21 @@ function squareInterpolation(fun, n)
 
 %siatka dla funkcji
 step = 1/n;
-[X,Y] = meshgrid(-1:step:1,-1:step:1);
+[X,Y] = meshgrid(-1:step:1,1:-step:-1);
+A = zeros(4*n*n, 12); %kazdy wiersz zawiera x1,y1,x2,y2,x3,y3,oraz srodki bokow
+B = zeros(4*n*n, 5); %wektor wynikowy - wspolrzedne srodka ciezkosci, wartosc funkcji, przyblizenia oraz blad.
 
 [k,~] = size(X);
-for i=1:k
-    for j=1:k
-        if(abs(X(i,j)) + abs(Y(i,j)) > 1)
-            X(i,j) = NaN;
-            Y(i,j) = NaN;
-        end            
-    end
-end
+% for i=1:k
+%     for j=1:k
+%         if(abs(X(i,j)) + abs(Y(i,j)) > 1)
+%             X(i,j) = NaN;
+%             Y(i,j) = NaN;
+%         end            
+%     end
+% end
 
+counter = 1;
 for i=1:floor(k/2)
     even = false;
     length = i*2-1;
@@ -22,16 +25,23 @@ for i=1:floor(k/2)
     for j = 1+offset:k-offset
         if(even)
             %left
-            T = [i,j-1,i,j,i+1,j]
+            T = [i,j-1,i,j,i+1,j];
+            A(counter,:) = initializeAfromT(T,X,Y);
+            counter = counter + 1;
             %right
-            T = [i,j,i,j,i+1,j+1]
+            T = [i,j,i,j,i+1,j+1];
+            A(counter,:) = initializeAfromT(T,X,Y);
         else
             %left
-            T = [i,j,i+1,j-1,i+1,j]
+            T = [i,j,i+1,j-1,i+1,j];
+            A(counter,:) = initializeAfromT(T,X,Y);
+            counter = counter + 1;
             %right
-            T = [i,j,i+1,j,i+1,j+1]
+            T = [i,j,i+1,j,i+1,j+1];
+            A(counter,:) = initializeAfromT(T,X,Y);
         end
             
+        counter = counter + 1;
         even = ~even;  
     end
 end
@@ -43,19 +53,54 @@ for i=ceil(k/2)+1:k
     for j = 1+offset:k-offset
         if(even)
             %left
-            T = [i,j-1,i,j,i-1,j]
+            T = [i,j-1,i,j,i-1,j];
+            A(counter,:) = initializeAfromT(T,X,Y);
+            counter = counter + 1;
             %right
-            T = [i,j,i,j,i-1,j+1]
+            T = [i,j,i,j,i-1,j+1];
+            A(counter,:) = initializeAfromT(T,X,Y);
         else
             %left
-            T = [i,j,i-1,j-1,i-1,j]
+            T = [i,j,i-1,j-1,i-1,j];
+            A(counter,:) = initializeAfromT(T,X,Y);
+            counter = counter + 1;
             %right
-            T = [i,j,i-1,j,i-1,j+1]
+            T = [i,j,i-1,j,i-1,j+1];
+            A(counter,:) = initializeAfromT(T,X,Y);
         end
             
+        counter = counter + 1;
         even = ~even;  
     end
 end
 
+for i=1:4*n*n
+    %wlasciwa interpolacja
+    x = (A(i,1) + A(i,3) + A(i,5))/3;
+    y = (A(i,2) + A(i,4) + A(i,6))/3;
+    
+    B(i,1) = x;
+    B(i,2) = y; 
+    B(i,3) = fun(x,y);
+    
+    b = zeros(6,1);
+    M = zeros(6,6);
+    
+    for j=1:6
+        b(j) = fun(A(i,2*j-1), A(i,2*j));
+        
+        xj = A(i,2*j-1);
+        yj = A(i,2*j);
+        M(j,:) = [1, xj, yj, xj*yj, xj*xj, yj*yj];
+    end
+    
+    c = M\b;
+    
+    fApprox = c(1) + c(2)*x + c(3)*y + c(4)*x*y + c(5)*x*x + c(6)*y*y;
+    B(i,4) = fApprox;
+    B(i,5) = abs(fApprox - B(i,3));
+end
+
+disp(B);
 end
 
