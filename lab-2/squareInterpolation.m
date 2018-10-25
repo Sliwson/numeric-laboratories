bf8e8663b9b3d1af9b82f046c892d05a105470f7
Sliwson <mateusz_sliw@interia.pl>
2018-10-25 10:53:46 +0200

    Add some comments

--- lab-2/squareInterpolation.m
+++ lab-2/squareInterpolation.m
@@ -1,14 +1,19 @@
 function squareInterpolation(fun, n)
-%SQUAREINTERPOLATION 
+%SQUAREINTERPOLATION funkcja interpoluje funkcje dwoch zmiennych fun, na obszarze
+%|x|+|y| <= 0 podzielonym na 4*n*n trojkatow przystajacych.
+%   fun - funkcja dwoch zmiennych
+%   n - parametr podzialu obszaru
 
 %siatka dla funkcji
 step = 1/n;
 [X,Y] = meshgrid(-1:step:1,1:-step:-1);
+
 A = zeros(4*n*n, 12); %kazdy wiersz zawiera x1,y1,x2,y2,x3,y3,oraz srodki bokow
 B = zeros(4*n*n, 5); %wektor wynikowy - wspolrzedne srodka ciezkosci, wartosc funkcji, przyblizenia oraz blad.
 
 [k,~] = size(X);
 
+%petla wyznaczajaca podzial obszaru na trojkaty
 counter = 1;
 for i=1:floor(k/2)
     even = false;
@@ -68,6 +73,8 @@ end
 
 for i=1:4*n*n
     %wlasciwa interpolacja
+    
+    %srodek ciezkosci trojkata
     x = (A(i,1) + A(i,3) + A(i,5))/3;
     y = (A(i,2) + A(i,4) + A(i,6))/3;
     
@@ -75,6 +82,7 @@ for i=1:4*n*n
     B(i,2) = y; 
     B(i,3) = fun(x,y);
     
+    %wyznaczenie macierzy, dzieki ktorej obliczymy wspolczynniki
     b = zeros(6,1);
     M = zeros(6,6);
     
@@ -94,5 +102,7 @@ for i=1:4*n*n
 end
 disp('    x -- y -- f(x,y) -- fApprox(x,y) -- error');
 disp(B);
+disp('Blad maksymalny: ');
+disp(max(B(:,5)));
 end
 
