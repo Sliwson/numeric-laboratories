0ae790fdce91f5e8e0b4d24c48e4a606bca36968
Sliwson <mateusz_sliw@interia.pl>
2018-11-01 18:59:22 +0100

    Add more examples

--- lab-2/testInterpolation.m
+++ lab-2/testInterpolation.m
@@ -8,4 +8,18 @@ testFourCases(f);
 
 %funkcja której pochodna w naszym obszarze jest nieograniczona.
 f = @(x,y) tan (2*x + 3*y);
-testFourCases(f)
+testFourCases(f);
+
+%wyznaczenie wspolczynnnika zbieznosci
+disp('===============TEST CASE===============');
+f = @(x,y) (4.*cos(x) + 2.*y.*y - 4.*x.^5)/(2.*x.^2 + 3 + 8*y.^2);
+j = [2,4,8,16,32,64,128,256];
+
+
+[~,err1] = squareInterpolation(f,1);
+
+for i = 1:8
+    [~,err2] = squareInterpolation(f,j(i));
+    disp(err1/err2);
+    err1=err2;
+end
