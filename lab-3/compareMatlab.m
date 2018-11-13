e35ae7fb65ed8e125e3cbc595f71ab9bfc32eb83
Sliwson <mateusz_sliw@interia.pl>
2018-11-13 14:16:40 +0100

    Add function that display comparison neatly

---
+++ lab-3/compareMatlab.m
@@ -0,0 +1,14 @@
+function  compareMatlab(f,a,b,c,d,n,m)
+%COMPAREMATLAB Funkcja porownuje dzialanie naszej funkcji z wynikiem
+%uzyskanym funkcj? Matlaba
+result = trapezeInterpolation(f,a,b,c,d,n,m);
+resultMatlab = integral2(f,a,b,c,d);
+
+format long;
+disp("============================================");
+disp(f);
+disp(['result = ', num2str(result, 10)]);
+disp(['Matlab = ', num2str(resultMatlab, 10)]);
+disp(['roznica = ', num2str(result - resultMatlab)]);
+end
+
