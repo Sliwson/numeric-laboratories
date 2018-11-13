e3b3adfa006170cc50d9c02fd3d2ee3661cd3252
Sliwson <mateusz_sliw@interia.pl>
2018-11-13 14:23:02 +0100

    Add more details

--- lab-3/compareMatlab.m
+++ lab-3/compareMatlab.m
@@ -7,6 +7,8 @@ resultMatlab = integral2(f,a,b,c,d);
 format long;
 disp("============================================");
 disp(f);
+disp(['D = [',num2str(a),',',num2str(b),']x[',num2str(c),',',num2str(d),']']);
+disp(['n = ',num2str(n),', m = ',num2str(m)]);
 disp(['result = ', num2str(result, 10)]);
 disp(['Matlab = ', num2str(resultMatlab, 10)]);
 disp(['difference = ', num2str(result - resultMatlab)]);
