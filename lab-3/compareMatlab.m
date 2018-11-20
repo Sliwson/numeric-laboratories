522a50ad2dda82968f6ceae08c3a37d153d520ee
Sliwson <mateusz_sliw@interia.pl>
2018-11-20 19:10:25 +0100

    Add sym computations to report

--- lab-3/compareMatlab.m
+++ lab-3/compareMatlab.m
@@ -1,8 +1,11 @@
-function  compareMatlab(f,a,b,c,d,n,m)
+function  err = compareMatlab(f,a,b,c,d,n,m)
 %COMPAREMATLAB Funkcja porownuje dzialanie naszej funkcji z wynikiem
 %uzyskanym funkcj? Matlaba
 result = trapezeInterpolation(f,a,b,c,d,n,m);
 resultMatlab = integral2(f,a,b,c,d);
+
+syms x y;
+resultSymbolic = int(int(f,x,a,b),y,c,d);
 
 format long;
 disp("============================================");
@@ -11,6 +14,12 @@ disp(['D = [',num2str(a),',',num2str(b),']x[',num2str(c),',',num2str(d),']']);
 disp(['n = ',num2str(n),', m = ',num2str(m)]);
 disp(['result = ', num2str(result, 10)]);
 disp(['Matlab = ', num2str(resultMatlab, 10)]);
-disp(['difference = ', num2str(result - resultMatlab)]);
+try
+    disp(['Symbolic = ', num2str(double(resultSymbolic), 10)]);
+    err = abs(resultSymbolic-result);
+catch 
+    disp('Symbolic function cannot compute result');
+    err = -1;
+end
 end
 
