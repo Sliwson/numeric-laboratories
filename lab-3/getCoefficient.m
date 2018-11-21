929ad4e401eeaec4ba96530b09c605275e760436
Sliwson <mateusz_sliw@interia.pl>
2018-11-21 13:49:05 +0100

    Add comments to getCoefficient function

--- lab-3/getCoefficient.m
+++ lab-3/getCoefficient.m
@@ -1,6 +1,9 @@
 function coeff = getCoefficient(x,y,n,m)
-%GETCOEFFICIENT Summary of this function goes here
-%   Detailed explanation goes here
+%GETCOEFFICIENT Zwraca wspolczynnik macierzy C w metodzie trapezow.
+%x - wspolrzedna x
+%y - wspolrzedna y
+%n - ilosc podzialow wzdluz osi OX
+%m = ilosc podzialow wzlduz osi OY
 if (x == 1 || x == n+1)
     if(y == 1 || y==m+1)
         coeff = 1;
