a9b61c93ac5d1575efdf52acb615c1dd01915697
Sliwson <mateusz_sliw@interia.pl>
2018-12-13 17:21:26 +0100

    Improve tests

--- lab-4/test.m
+++ lab-4/test.m
@@ -5,14 +5,19 @@ n = 10;
 f = @(x,y) 2.*x + 3.*y;
 %presentResult(f,n,-2,2,-2,2);
 
-f = @(x,y) -5 + 4.*x - 2.*y + 3.*y.*y.*x.*x;
-%presentResult(f,n,-2,2,-2,2);
+f = @(x,y) -5 + 4.*x - 2.*y + 3.*y.*y.*x.*x + 1.5.*x.*x.*x.*x.*y - 3.*y.*y.*y;
+presentResult(f,n,-2,2,-2,2);
 
 f = @(x,y) sin(30./(x+y));
 %presentResult(f,n,1,3,1,3);
 
 f = @(x,y) sin(30./(x.*y.*y));
 %presentResult(f,100,1,3,1,3);
-
+    
 f = @(x,y) x.*x.*x.*y.*y;
-presentResult(f,100,-2,2,-2,2);
+f = @(x,y) sin(x.*x+y.*y);
+f = @(x,y) sin(5.*(x.*x+y.*y));
+%presentResult(f,10,-2,2,-2,2);
+%presentResult(f,20,-2,2,-2,2);
+%presentResult(f,30,-2,2,-2,2);
+%presentResult(f,40,-2,2,-2,2);
