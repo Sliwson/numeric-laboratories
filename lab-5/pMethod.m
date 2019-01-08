9b3a89d2685323cd02b5f4a69ffe90aaf9f13acb
Sliwson <mateusz_sliw@interia.pl>
2019-01-08 10:09:47 +0100

    Improve algorithm

--- lab-5/pMethod.m
+++ lab-5/pMethod.m
@@ -1,20 +1,29 @@
-function lambda = pMethod(v1, v2, v3, lambdaApproximation, iterations)
+function [lambda, iterations] = pMethod(v1, v2, v3, lambdaApproximation, precision, maxIterations)
 %PMETHOD Znajduje wartosc wlasna macierzy A znajdujaca sie najblizej
 %podanej wartosci wlasnej lambda. A jest trojdiagonalna i jej diagonale sa
 %przekazane jako parametry v1, v2, v3, gdzie v2 to diagonala glowna.
-%Obliczenia wykonywane sa dopoki nie zostanie osiagnieta liczba iteracji
-%(iterations)
-
+%Obliczenia wykonywane sa dopoki nie zostanie osiagnieta zadana precyzja
+%(precision)
 n = size(v2,2);
 v2 = v2 - lambdaApproximation*ones(1,n);
 
-x_prev = ones(n,1);
+x_prev = rand(n,1); %losowe przyblizenie poczatkowe
 
-for i=1:iterations
+lambdaPrev = lambdaApproximation;
+lambda = lambdaApproximation + 2*precision; %aby uniknac spelnienia warunku w pierwszym sprawdzeniu
+iterations = 0;
+
+while(abs(lambda-lambdaPrev) >= precision)
+    lambdaPrev = lambda;
     y_next = solveTridiagonal(v1,v2,v3,x_prev);
-    x_prev = y_next/norm(y_next);
+    s = dot(x_prev, y_next);
+    lambda = 1/s + lambdaApproximation;
+    x_prev = y_next/norm(y_next,2);
+    
+    iterations = iterations + 1;
+    
+    if(iterations > maxIterations)
+        break;
+    end
 end
-
-s = dot(x_prev, y_next);
-lambda = 1/s + lambdaApproximation;
 end
